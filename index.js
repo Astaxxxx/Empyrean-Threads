58f427753d707d17b3d93531bb77f452d39483d9
ByrneHub <116261123+ByrneHub@users.noreply.github.com>
2022-11-22 03:38:42 +0000

    added the new links for items on product page, fixed various product page issues

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // the is the responsive javascript hamburger viewport code -Alex
 
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger")
 
 hamburger.addEventListener("click", function () {
-  this.classList.toggle("is-active");
+  this.classList.toggle("is-active")
 })
 
 // some product item code i found
 
-var ProductImg = document.getElementByID("ProductImg");
-var SmallImg = Document.getElementByIdClassName("small-img");
+var ProductImg = document.getElementByID("ProductImg")
+var SmallImg = Document.getElementByIdClassName("small-img")
 
-SmallImg[0].onclick = function() 
-{
-  ProductImg.src = SmallImg[0].src;
+SmallImg[0].onclick = function () {
+  ProductImg.src = SmallImg[0].src
 }
-SmallImg[1].onclick = function() 
-{
-  ProductImg.src = SmallImg[1].src;
+SmallImg[1].onclick = function () {
+  ProductImg.src = SmallImg[1].src
 }
-SmallImg[2].onclick = function() 
-{
-  ProductImg.src = SmallImg[2].src;
+SmallImg[2].onclick = function () {
+  ProductImg.src = SmallImg[2].src
 }
-SmallImg[3].onclick = function() 
-{
-  ProductImg.src = SmallImg[3].src;
+SmallImg[3].onclick = function () {
+  ProductImg.src = SmallImg[3].src
 }
+
+var FaFa = document.getElementsByClassName("fa fa-indent")
+
+FaFa.onclick = function () {}
