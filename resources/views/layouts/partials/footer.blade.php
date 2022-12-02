6271407bf411cb214da1b0b1fb9e9d68ab8d60fb
210104451 <116261274+210104451@users.noreply.github.com>
2022-12-02 19:55:42 +0000

    Add files via upload

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,10 @@
+
+<link rel="stylesheet" href="{{asset('css/Pages/styles.css')}}">
+<footer>
+      Contact Us
+      <a href="contactus.html"
+        >Here! <br /><a href="RealEmpyreanThreads@gmail.com"
+          >RealEmpyreanThreads@gmail.com</a>
+          <p>&copy; All rights reserved. 2022</p>
+      </a>
+</footer>
